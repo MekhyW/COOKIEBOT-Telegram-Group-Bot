32d3cff6ba63df376d09ac8aafe852c7f53c1de0
Mekhy W.! <55763383+MekhyW@users.noreply.github.com>
2021-06-09 19:25:18 -0300

    IMW up again from 10 to 12

--- Bot/COOKIEBOT.py
+++ Bot/COOKIEBOT.py
@@ -46,7 +46,7 @@ messagespamlimit = 10
 limbotimespan = 600
 captchatimespan = 300
 intrometerpercentage = 1
-intrometerminimumwords = 10
+intrometerminimumwords = 12
 lowresolutionarea = 10000
 funfunctions = 1
 
@@ -795,7 +795,7 @@ def thread_function(msg):
                     if not os.path.isfile("Config_"+str(chat_id)+".txt"):
                         open("Config_"+str(chat_id)+".txt", 'a', encoding='utf-8').close()
                         text_file = open("Config_"+str(chat_id)+".txt", "w", encoding='utf-8')
-                        text_file.write("FurBots: 0\nSticker_Spam_Limit: 5\nMessage_Spam_Limit: 10\nTempo_sem_poder_mandar_imagem: 600\nTempo_Captcha: 300\nIntrometer_Percentage: 1\nIntrometer_minimum_words: 10\nLow_resolution_area: 10000\nFunções_Diversão: 1")
+                        text_file.write("FurBots: 0\nSticker_Spam_Limit: 5\nMessage_Spam_Limit: 10\nTempo_sem_poder_mandar_imagem: 600\nTempo_Captcha: 300\nIntrometer_Percentage: 1\nIntrometer_minimum_words: 12\nLow_resolution_area: 10000\nFunções_Diversão: 1")
                         text_file.close()
                     text_file = open("Config_"+str(chat_id)+".txt", "r", encoding='utf-8')
                     lines = text_file.readlines()
