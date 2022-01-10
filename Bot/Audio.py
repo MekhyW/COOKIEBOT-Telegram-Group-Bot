5527b51a8216d6ceb4114e649f61fa40b1fb95c1
Mekhy W.! <55763383+MekhyW@users.noreply.github.com>
2022-01-10 18:54:37 -0300

    New speech-to-text engine (google cloud w/ punctuator), minimum_words filter, confidence filter

--- Bot/Audio.py
+++ Bot/Audio.py
@@ -1,7 +1,12 @@
 from universal_funcs import *
 import subprocess
-import speech_recognition, ShazamAPI
-recognizer = speech_recognition.Recognizer()
+import ShazamAPI
+from google.cloud import speech
+from google.oauth2 import service_account
+credentials = service_account.Credentials.from_service_account_file('cookiebot_cloudserviceaccount.json')
+client = speech.SpeechClient(credentials=credentials)
+minimum_words_STT = 4
+confidence_threshold = 0.75
 
 def Identify_music(cookiebot, msg, chat_id, AUDIO_FILE):
     shazam = ShazamAPI.Shazam(open(AUDIO_FILE, 'rb').read())
@@ -11,18 +16,31 @@ def Identify_music(cookiebot, msg, chat_id, AUDIO_FILE):
         cookiebot.sendMessage(chat_id, "MÚSICA: 🎵 " + response[1]['track']['title'] + " - " + response[1]['track']['subtitle'] + " 🎵", reply_to_message_id=msg['message_id'])
 
 def Speech_to_text(cookiebot, msg, chat_id):
-    cookiebot.sendChatAction(chat_id, 'typing')
+    global minimum_words_STT
+    global confidence_threshold
     r = requests.get("https://api.telegram.org/file/bot{}/{}".format(cookiebotTOKEN, cookiebot.getFile(msg['voice']['file_id'])['file_path']), allow_redirects=True)
     open('VOICEMESSAGE.oga', 'wb').write(r.content)
     subprocess.run(['ffmpeg', '-hide_banner', '-loglevel', 'panic', '-i', 'VOICEMESSAGE.oga', "VOICEMESSAGE.wav", '-y'])
     AUDIO_FILE = "VOICEMESSAGE.wav"
     try:
-        with speech_recognition.AudioFile(AUDIO_FILE) as source:
-            audio = recognizer.record(source)
-        voicetext_ptbr = recognizer.recognize_google(audio, language="pt-BR", show_all=True)['alternative'][0]
-        #voicetext_enus = recognizer.recognize_google(audio, language="en-US", show_all=True)['alternative'][0]
-        Text = voicetext_ptbr['transcript'].capitalize()
-        cookiebot.sendMessage(chat_id, "Texto: \n"+'"'+Text+'"', reply_to_message_id=msg['message_id'])
+        cookiebot.sendChatAction(chat_id, 'typing')
+        with open('VOICEMESSAGE.wav', "rb") as audio_file:
+            content = audio_file.read()
+            audio_file.close()
+        audio = speech.RecognitionAudio(content=content)
+        config = speech.RecognitionConfig(language_code="pt-BR", enable_automatic_punctuation=True)
+        response = client.recognize(config=config, audio=audio)
+        Text = ''
+        for i, result in enumerate(response.results):
+            alternative = result.alternatives[0]
+            if alternative.confidence < confidence_threshold:
+                return
+            Text += alternative.transcript.capitalize()
+            if i < len(response.results) - 1:
+                Text += '\n'
+        if len(Text.split()) >= minimum_words_STT:
+            cookiebot.sendChatAction(chat_id, 'typing')
+            cookiebot.sendMessage(chat_id, 'Texto:\n"{}"'.format(Text), reply_to_message_id=msg['message_id'])
     except Exception as e:
         print(e)
     Identify_music(cookiebot, msg, chat_id, AUDIO_FILE)
