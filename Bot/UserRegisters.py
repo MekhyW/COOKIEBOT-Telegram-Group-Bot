497fd8f4519b4b81d0f5c15d86b753cf90c0e3ae
Mekhy W.! <55763383+MekhyW@users.noreply.github.com>
2022-02-15 08:42:46 -0300

    Update UserRegisters.py

--- Bot/UserRegisters.py
+++ Bot/UserRegisters.py
@@ -83,17 +83,21 @@ def Quem(cookiebot, msg, chat_id):
 
 def Shippar(cookiebot, msg, chat_id):
     cookiebot.sendChatAction(chat_id, 'typing')
-    wait_open("Registers/"+str(chat_id)+".txt")
-    text_file = open("Registers/"+str(chat_id)+".txt", "r+", encoding='utf-8')
-    lines = text_file.readlines()
-    text_file.close()
-    targetA = None
-    targetB = None
-    while len(lines)>2 and (targetA in (None, '') and targetB in (None, '') or targetA == targetB):
-        targetA = lines[random.randint(0, len(lines)-1)].replace("\n", '')
-        targetA = targetA.split()[0]
-        targetB = lines[random.randint(0, len(lines)-1)].replace("\n", '')
-        targetB = targetB.split()[0]
+    if len(msg['text'].split()) >= 3:
+        targetA = msg['text'].split()[1]
+        targetB = msg['text'].split()[2]
+    else:
+        wait_open("Registers/"+str(chat_id)+".txt")
+        text_file = open("Registers/"+str(chat_id)+".txt", "r+", encoding='utf-8')
+        lines = text_file.readlines()
+        text_file.close()
+        targetA = None
+        targetB = None
+        while len(lines)>2 and (targetA in (None, '') and targetB in (None, '') or targetA == targetB):
+            targetA = lines[random.randint(0, len(lines)-1)].replace("\n", '')
+            targetA = targetA.split()[0]
+            targetB = lines[random.randint(0, len(lines)-1)].replace("\n", '')
+            targetB = targetB.split()[0]
     divorce_prob = str(random.randint(0, 100))
     couple_characteristic = random.choice(['Eles se tratam sempre com respeito, educação e bondade', 'Eles evitam julgamentos precipitados ou tentam mudar a personalidade do outro', 'As diferenças existem como em todas as relações, mas elas são respeitadas e superadas', 'Cada um se responsabiliza por seus próprios atos e sentimentos e não culpa o outro por possíveis frustrações e desilusões', 'Eles sempre se certificam de que estão passando tempo suficiente juntos', 'Vocês conseguem rir um do outro e raramente ficam constrangidos em situações que poderiam ser embaraçosas para grande parte dos casais românticos', 
     'Vocês saem juntos por prazer e nunca por obrigação, já que compartilham dos mesmos gostos e preferências', 'Vocês conseguem se comunicar e se entender com simples trocas de olhares, sem precisar verbalizar o que sentem no momento', 'Mesmo quando tentam disfarçar, sempre sabem exatamente o que o outro está pensando', 'Uma tarde chuvosa em casa se transforma em um excelente programa a dois', 'O silêncio, quando surge entre vocês, nunca é incômodo ou desafiador', 'Vocês possuem um número infinito de piadas internas', 'Vocês não se sentem constrangidos ou intimidados em contar segredos ou mesmo chorar um na frente do outro', 'Quando um dos parceiros está triste, o outro sabe exatamente o que dizer e o que fazer para afastar o sentimento ruim',
