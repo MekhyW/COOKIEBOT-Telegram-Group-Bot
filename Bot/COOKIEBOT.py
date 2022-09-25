d039775dbc2a830cbb29b732641c4fc9aeb0e259
Mekhy W.! <55763383+MekhyW@users.noreply.github.com>
2022-09-25 17:00:46 -0300

    Autopublisher V2 - part 1

--- Bot/COOKIEBOT.py
+++ Bot/COOKIEBOT.py
@@ -51,6 +51,7 @@ def thread_function(msg):
                 FurBots, sfw, stickerspamlimit, limbotimespan, captchatimespan, funfunctions, utilityfunctions, language = GetConfig(chat_id)
                 CheckNewName(msg, chat_id)
                 lastmessagedate, lastmessagetime = CheckLastMessageDatetime(msg, chat_id)
+                RegisterActivity(msg, chat_id)
             if content_type == "new_chat_member":
                 if 'username' in msg['new_chat_participant'] and msg['new_chat_participant']['username'] in ["MekhysBombot", "CookieMWbot"]:
                     wait_open("Blacklist.txt")
@@ -83,9 +84,13 @@ def thread_function(msg):
                 if sfw == 1:
                     photo_id = msg['photo'][-1]['file_id']
                     AddtoRandomDatabase(msg, chat_id, photo_id)
+                if 'sender_chat' in msg and 'text' in msg:
+                    AskPublisher(cookiebot, msg, chat_id, language)
             elif content_type == "video":
                 if sfw == 1:
                     AddtoRandomDatabase(msg, chat_id)
+                if 'sender_chat' in msg and 'text' in msg:
+                    AskPublisher(cookiebot, msg, chat_id, language)
             elif content_type == "document":
                 if sfw == 1:
                     AddtoRandomDatabase(msg, chat_id)
@@ -224,6 +229,19 @@ def handle_query(msg):
     query_id, from_id, query_data = telepot.glance(msg, flavor='callback_query')
     if 'CONFIG' in query_data:
         ConfigVariableButton(cookiebot, msg, query_data)
+    elif 'Pub' in query_data:
+        if query_data.startswith('Approve'):
+            origin_chatid = query_data.split()[1].split('-')[0]
+            origin_messageid = query_data.split()[1].split('-')[1]
+            with open('PostQueue.txt', 'a', encoding='utf-8') as f:
+                f.write(origin_chatid + ' ' + origin_messageid + '\n')
+            f.close()
+            try:
+                Send(cookiebot, "Adicionei seu post à fila!", from_id)
+            except:
+                Send(cookiebot, "Seu post foi adicionado à fila, mas não consegui te mandar uma mensagem no privado.\nMe mande /start no privado", origin_chatid)
+        else:
+            cookiebot.deleteMessage(telepot.message_identifier(msg['message']))
     else:
         listaadmins_id = []
         for admin in cookiebot.getChatAdministrators(msg['message']['reply_to_message']['chat']['id']):
--- Bot/Publisher.py
+++ Bot/Publisher.py
@@ -1,1 +1,42 @@
 from universal_funcs import *
+
+def AskPublisher(cookiebot, msg, chat_id, language):
+    cookiebot.sendMessage(chat_id, "Publish post?", reply_to_message_id=msg['message_id'], 
+    reply_markup=InlineKeyboardMarkup(inline_keyboard=[
+            [InlineKeyboardButton(text="✔️",callback_data='ApprovePub {}-{}'.format(str(chat_id)))],
+            [InlineKeyboardButton(text="❌",callback_data='DenyPub')]
+        ]
+    ))
+
+def RestartActivityRegister(date, chat_id):
+    with open('MessageActivity/{}.txt'.format(str(chat_id)), 'w') as f:
+        f.write(date+'\n')
+        for bin in range(24*60):
+            f.write(str(bin) + ': 0\n')
+    f.close()
+
+def RegisterActivity(msg, chat_id):
+    messageTime = datetime.datetime.utcfromtimestamp(msg['date'])
+    messageTime = messageTime.strftime('%Y-%m-%d %H:%M:%S')
+    date = messageTime.split()[0]
+    time = messageTime.split()[1].split(':')
+    hour = int(time[0])
+    minute = int(time[1])
+    bin = hour*60 + minute
+    if not os.path.exists('MessageActivity/{}.txt'.format(str(chat_id))):
+        RestartActivityRegister(date, chat_id)
+    wait_open('MessageActivity/{}.txt'.format(str(chat_id)))
+    with open('MessageActivity/{}.txt'.format(str(chat_id)), 'r') as f:
+        lines = f.readlines()
+    f.close()
+    if line[0] != date:
+        RestartActivityRegister(date, chat_id)
+    with open('MessageActivity/{}.txt'.format(str(chat_id)), 'w') as f:
+        for line in lines:
+            if line.startswith(str(bin)+':'):
+                line = line.split(' ')
+                line[1] = str(int(line[1]) + 1)
+                line = ' '.join(line)
+            f.write(line)
+        f.truncate()
+    f.close()
