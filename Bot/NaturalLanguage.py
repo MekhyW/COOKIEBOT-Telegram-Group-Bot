aed4bc2661bac20202b1f11932c697bdb80f7848
Mekhy W.! <55763383+MekhyW@users.noreply.github.com>
2022-10-01 22:04:48 -0300

    Update NaturalLanguage.py

--- Bot/NaturalLanguage.py
+++ Bot/NaturalLanguage.py
@@ -34,13 +34,8 @@ def InteligenciaArtificial(cookiebot, msg, chat_id, language):
         AnswerFinal = "?"
     else:
         Answer1 = ''
-        if language == "pt":
-            r = requests.post('https://wsapi.simsimi.com/190410/talk', headers={'x-api-key': smalltalkKey}, json={'utext': message, 'lang': 'pt', "atext_bad_prob_max": 1.0})
-        elif language == "es":
-            r = requests.post('https://wsapi.simsimi.com/190410/talk', headers={'x-api-key': smalltalkKey}, json={'utext': message, 'lang': 'es', "atext_bad_prob_max": 1.0})
-        else:
-            r = requests.post('https://wsapi.simsimi.com/190410/talk', headers={'x-api-key': smalltalkKey}, json={'utext': message, 'lang': 'en', "atext_bad_prob_max": 1.0})
-        Answer1 = json.loads(r.text)['atext'].capitalize()
+        r = requests.get('https://simsimi.info/api/?text=olá&lc={}'.format(language))
+        Answer1 = json.loads(r.text)['message'].capitalize()
         if Answer1 and "Eu não resposta." not in Answer1 and "I don't know what you're saying." not in Answer1:
             AnswerFinal = Answer1
         else:
