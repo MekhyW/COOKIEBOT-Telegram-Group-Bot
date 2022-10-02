b60bbccf7f3432038dd881804eff26c60a7f4522
Mekhy W.! <55763383+MekhyW@users.noreply.github.com>
2022-10-01 22:32:23 -0300

    Update COOKIEBOT.py

--- Bot/COOKIEBOT.py
+++ Bot/COOKIEBOT.py
@@ -181,7 +181,7 @@ def thread_function(msg):
                     Quem(cookiebot, msg, chat_id, language)
                 elif 'reply_to_message' in msg and 'photo' in msg['reply_to_message'] and 'caption' in msg['reply_to_message'] and str(round(captchatimespan/60)) in msg['reply_to_message']['caption']:
                     SolveCaptcha(cookiebot, msg, chat_id, False, limbotimespan, language)
-                elif (('reply_to_message' in msg and msg['reply_to_message']['from']['first_name'] == 'Cookiebot') or "cookiebot" in msg['text'].lower() or "@CookieMWbot" in msg['text']) and funfunctions == True:
+                elif (('reply_to_message' in msg and msg['reply_to_message']['from']['first_name'] == 'Cookiebot' and 'text' in msg['reply_to_message']) or "cookiebot" in msg['text'].lower() or "@CookieMWbot" in msg['text']) and funfunctions == True:
                     if not OnSay(cookiebot, msg, chat_id):
                         AnswerFinal = InteligenciaArtificial(cookiebot, msg, chat_id, language)
                         cookiebot.sendMessage(chat_id, AnswerFinal, reply_to_message_id=msg['message_id'])
