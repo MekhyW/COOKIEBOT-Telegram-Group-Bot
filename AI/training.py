f05b23b22387405058649df636dcc08d774aecfa
Mekhy W.! <55763383+MekhyW@users.noreply.github.com>
2022-10-25 23:36:25 -0300

    Further training

--- AI/training.py
+++ AI/training.py
@@ -13,7 +13,7 @@ bot = ChatBot(
 conversa = ListTrainer(bot)
 df = pd.read_csv('result-master-ptbr.csv')
 for index in range(len(df)):
-    if index < 110000:
+    if index < 155000:
         continue
     print(str(index) + ' / ' + str(len(df)))
     try:
