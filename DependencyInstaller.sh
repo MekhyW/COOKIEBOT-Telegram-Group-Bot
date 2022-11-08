0ef4fe5de5c050efe6ce7000ddb19c2c7e71b50c
Mekhy W.! <55763383+MekhyW@users.noreply.github.com>
2022-11-08 09:38:55 -0300

    Update DependencyInstaller.sh

--- DependencyInstaller.sh
+++ DependencyInstaller.sh
@@ -22,11 +22,13 @@ pip3 install captcha
 pip3 install google-cloud-speech
 pip3 install ShazamAPI
 pip3 install opencv-python
-pip3 install ChatterBot
 pip3 install ChatterBot-corpus
 pip3 install awscli==1.11.18
 pip3 install SQLAlchemy==1.3.6
 pip3 install Mako==1.1.2
 pip3 install levenshtein
 pip3 install spacy==2.3.5
+pip3 install ChatterBot==1.0.4
+pip3 install pytz
 python3 -m spacy download en
+sudo apt install ffmpeg
