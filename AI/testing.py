30de8a52c082ec39712976a9689ab2dd35fc166d
Mekhy W.! <55763383+MekhyW@users.noreply.github.com>
2022-11-13 21:47:44 -0300

    Reverted changes

--- AI/testing.py
+++ AI/testing.py
@@ -1,13 +1,10 @@
 from chatterbot import ChatBot
-from chatterbot import comparisons
-from chatterbot import response_selection
 bot = ChatBot(
     'Cookiebot_AI',  
     logic_adapters=[
         {
             'import_path': 'chatterbot.logic.BestMatch',
-            'statement_comparison_function': comparisons.LevenshteinDistance,
-            'response_selection_method': response_selection.get_most_frequent_response
+            'statement_comparison_function': 'chatterbot.comparisons.LevenshteinDistance',
         }
     ],
     preprocessors=[
