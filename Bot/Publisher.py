9845aa51a089a5a8cf88fcad713189982d337e98
Mekhy W.! <55763383+MekhyW@users.noreply.github.com>
2022-11-20 22:49:22 -0300

    Sending job_data as byte array

--- Bot/Publisher.py
+++ Bot/Publisher.py
@@ -39,7 +39,7 @@ def create_job(job_name, job_description, job_data, job_schedule):
         'description': job_description,
         'pubsub_target': {
             'topic_name': topic_name,
-            'data': job_data,
+            'data': b'{}'.format(job_data),
         },
         'schedule': job_schedule,
     }
