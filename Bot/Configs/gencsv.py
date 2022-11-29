751b80a982b41f32a5081e89320d9b3fae439d0d
Felipe Catapano Emrich Melo <mefkhytheroboticist@gmail.com>
2022-11-29 09:31:33 -0300

    Update gencsv.py

--- Bot/Configs/gencsv.py
+++ Bot/Configs/gencsv.py
@@ -14,7 +14,7 @@ for file in os.listdir():
         id = file.replace('.txt', '').replace('Config_', '')
         for line in lines:
             if line.startswith('FurBots'):
-                furbots = bool(int(line.replace('FurBots: ', '').replace('\n', '')))
+                furbots = str(bool(int(line.replace('FurBots: ', '').replace('\n', '')))).lower()
             elif line.startswith('Sticker_Spam_Limit'):
                 stickerSpamLimit = int(line.replace('Sticker_Spam_Limit: ', '').replace('\n', ''))
             elif line.startswith('Tempo_sem_poder_mandar_imagem'):
@@ -22,17 +22,17 @@ for file in os.listdir():
             elif line.startswith('Tempo_Captcha'):
                 timeCaptcha = int(line.replace('Tempo_Captcha: ', '').replace('\n', ''))
             elif line.startswith('Funções_Diversão'):
-                functionsFun = bool(int(line.replace('Funções_Diversão: ', '').replace('\n', '')))
+                functionsFun = str(bool(int(line.replace('Funções_Diversão: ', '').replace('\n', '')))).lower()
             elif line.startswith('Funções_Utilidade'):
-                functionsUtility = bool(int(line.replace('Funções_Utilidade: ', '').replace('\n', '')))
+                functionsUtility = str(bool(int(line.replace('Funções_Utilidade: ', '').replace('\n', '')))).lower()
             elif line.startswith('SFW'):
-                sfw = bool(int(line.replace('SFW: ', '').replace('\n', '')))
+                sfw = str(bool(int(line.replace('SFW: ', '').replace('\n', '')))).lower()
             elif line.startswith('Language'):
                 language = line.replace('Language: ', '').replace('\n', '')
             elif line.startswith('Publisher_Post'):
-                publisherPost = bool(int(line.replace('Publisher_Post: ', '').replace('\n', '')))
+                publisherPost = str(bool(int(line.replace('Publisher_Post: ', '').replace('\n', '')))).lower()
             elif line.startswith('Publisher_Ask'):
-                publisherAsk = bool(int(line.replace('Publisher_Ask: ', '').replace('\n', '')))
+                publisherAsk = str(bool(int(line.replace('Publisher_Ask: ', '').replace('\n', '')))).lower()
         df = df.append({'id': id, 'furbots': furbots, 'stickerSpamLimit': stickerSpamLimit, 'timeWithoutSendingImages': timeWithoutSendingImages, 'timeCaptcha': timeCaptcha, 'functionsFun': functionsFun, 'functionsUtility': functionsUtility, 'sfw': sfw, 'language': language, 'publisherPost': publisherPost, 'publisherAsk': publisherAsk}, ignore_index=True)
 
 print(df.head(100))
