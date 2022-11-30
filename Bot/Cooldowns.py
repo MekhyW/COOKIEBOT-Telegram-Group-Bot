40767ae9527ed4a7478488fe21f314cbea238290
Felipe Catapano Emrich Melo <mefkhytheroboticist@gmail.com>
2022-11-30 13:15:21 -0300

    Update Cooldowns.py

--- Bot/Cooldowns.py
+++ Bot/Cooldowns.py
@@ -3,47 +3,23 @@ from universal_funcs import *
 sentcooldownmessage = False
 
 def Sticker_anti_spam(cookiebot, msg, chat_id, stickerspamlimit, language):
-    wait_open("Stickers.txt")
-    text_file = open("Stickers.txt", "r+", encoding='utf8')
-    lines = text_file.readlines()
-    if any(str(chat_id) in string for string in lines):
-        pass
+    sticker_seq = GetRequestBackend(f"stickers/{chat_id}")
+    if 'error' in sticker_seq and sticker_seq['error'] == "Not Found":
+        PostRequestBackend(f"stickers/{chat_id}", {"id": chat_id, "lastUsed": 0})
     else:
-        lines.append("\n"+str(chat_id)+" 0")
-    text_file.close()
-    counter_new = 0
-    for line in lines:
-        if str(chat_id) in line:
-            counter_new = int(line.split()[1])+1
-            break
-    text_file = open("Stickers.txt", "w", encoding='utf8')
-    for line in lines:
-        if str(chat_id) in line:
-            text_file.write(line.split()[0] + " " + str(int(line.split()[1])+1) + "\n")
-        else:
-            text_file.write(line)
-    text_file.close()
-    if counter_new == stickerspamlimit:
-        Send(cookiebot, chat_id, "Cuidado com o flood de stickers.\nMantenham o chat com textos!", msg, language)
-    if counter_new > stickerspamlimit:
-        DeleteMessage(cookiebot, telepot.message_identifier(msg))
+        lastUsed = int(sticker_seq['lastUsed'])
+        if lastUsed == stickerspamlimit:
+            Send(cookiebot, chat_id, "Cuidado com o flood de stickers.\nMantenham o chat com textos!", msg, language)
+        if lastUsed > stickerspamlimit:
+            DeleteMessage(cookiebot, telepot.message_identifier(msg))
 
 def CmdCooldownUpdates(msg, chat_id, lastmessagetime):
+    global sentcooldownmessage
     if float(lastmessagetime)+60 < ((datetime.datetime.now().hour*3600)+(datetime.datetime.now().minute*60)+(datetime.datetime.now().second)):
         sentcooldownmessage = False
 
 def StickerCooldownUpdates(msg, chat_id):
-    wait_open("Stickers.txt")
-    text_file = open("Stickers.txt", "r+", encoding='utf8')
-    lines = text_file.readlines()
-    text_file.close()
-    text_file = open("Stickers.txt", "w", encoding='utf8')
-    for line in lines:
-        if str(chat_id) in line:
-            text_file.write(line.split()[0] + " " + "0" + "\n")
-        else:
-            text_file.write(line)
-    text_file.close()
+    PutRequestBackend(f"stickers/{chat_id}", {"lastUsed": 0})
 
 def CooldownAction(cookiebot, msg, chat_id, language):
     global sentcooldownmessage
