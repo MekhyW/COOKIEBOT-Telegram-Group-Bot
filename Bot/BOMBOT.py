a5dcec5446957d8f4f45c3c8a313d4031eb924e8
Felipe Catapano Emrich Melo <mefkhytheroboticist@gmail.com>
2022-12-13 16:03:16 -0300

    Removed OnSay

--- Bot/BOMBOT.py
+++ Bot/BOMBOT.py
@@ -179,13 +179,11 @@ def thread_function(msg):
                 elif 'reply_to_message' in msg and 'photo' in msg['reply_to_message'] and 'caption' in msg['reply_to_message'] and str(round(captchatimespan/60)) in msg['reply_to_message']['caption']:
                     SolveCaptcha(cookiebot, msg, chat_id, False, limbotimespan, language)
                 elif (('reply_to_message' in msg and msg['reply_to_message']['from']['first_name'] == 'Cookiebot' and 'text' in msg['reply_to_message']) or "cookiebot" in msg['text'].lower() or "@CookieMWbot" in msg['text']) and funfunctions == True:
-                    if not OnSay(cookiebot, msg, chat_id):
-                        AnswerFinal = InteligenciaArtificial(cookiebot, msg, chat_id, language)
-                        cookiebot.sendMessage(chat_id, AnswerFinal, reply_to_message_id=msg['message_id'])
+                    AnswerFinal = InteligenciaArtificial(cookiebot, msg, chat_id, language)
+                    cookiebot.sendMessage(chat_id, AnswerFinal, reply_to_message_id=msg['message_id'])
                 else:
                     SolveCaptcha(cookiebot, msg, chat_id, False, limbotimespan, language)
                     CheckCaptcha(cookiebot, msg, chat_id, captchatimespan, language)
-                    OnSay(cookiebot, msg, chat_id)
             if chat_type != 'private' and 'text' in msg:
                 StickerCooldownUpdates(msg, chat_id)
             run_unnatendedthreads()
