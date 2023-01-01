23a18c1a82677b8abf0dce1aa29f48824f271fa0
Felipe Catapano Emrich Melo <mefkhytheroboticist@gmail.com>
2023-01-01 10:45:29 -0300

    Python 3.9 --> 3.11

--- DependencyInstaller.sh
+++ DependencyInstaller.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 #
 # This script will install all the dependencies for the project.
+# This script is made for Ubuntu 22.04 LTS x86_64
 #
 # Usage:
 # ./DependencyInstaller.sh
 #
 # -----------------------------------------------------------------------------
+sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt-get update
+sudo apt install python3.11
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 1
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.11 2
+sudo update-alternatives --config python3
 sudo apt-get install -y git-all
 sudo apt-get install -y python3-pip
 sudo apt-get install -y libfreetype6-dev
@@ -26,7 +32,6 @@ pip3 install google-cloud-pubsub
 pip3 install google-cloud-scheduler
 pip3 install google-cloud-vision
 pip3 install ShazamAPI
-pip3 install numpy
 pip3 install opencv-python
 pip3 install forex-python
 pip3 install price-parser
@@ -37,5 +42,5 @@ pip3 install Mako==1.1.2
 pip3 install levenshtein
 pip3 install ChatterBot==1.0.4
 pip3 install pytz
-pip3 install spacy==2.3.5
+pip3 install spacy
 python3 -m spacy download en
