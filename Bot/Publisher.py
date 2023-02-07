e066332df4bb7babee866c5c9f6dcd8b038e7f42
Felipe Catapano Emrich Melo <mefkhytheroboticist@gmail.com>
2023-02-07 10:51:24 -0300

    Publisher always convert to USD if not already USD

--- Bot/Publisher.py
+++ Bot/Publisher.py
@@ -117,9 +117,12 @@ def ConvertPricesinText(text, code_target):
             else:
                 final_text += f"{paragraph}\n"
                 continue
-            if code_from != code_target:
+            if code_from != code_target or code_from != 'USD':
                 try:
-                    rate = json.loads(requests.get(f"https://v6.exchangerate-api.com/v6/{exchangerate_key}/latest/{code_from}").text)['conversion_rates'][code_target]
+                    if code_from != 'USD':
+                        rate = json.loads(requests.get(f"https://v6.exchangerate-api.com/v6/{exchangerate_key}/latest/{code_from}").text)['conversion_rates']['USD']
+                    else:
+                        rate = json.loads(requests.get(f"https://v6.exchangerate-api.com/v6/{exchangerate_key}/latest/{code_from}").text)['conversion_rates'][code_target]
                     converted = round(parsed.amount_float * rate, 2)
                     final_text += f"{paragraph} ({code_target} ≈{converted})\n"
                 except Exception as e:
