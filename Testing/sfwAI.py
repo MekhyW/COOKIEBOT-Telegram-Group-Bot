04aa06cb23b3c49a97f7f14dc3629d4c791be231
Felipe Catapano Emrich Melo <mefkhytheroboticist@gmail.com>
2023-04-24 20:25:35 -0300

    Attempt to make AI less verbose

--- Testing/sfwAI.py
+++ Testing/sfwAI.py
@@ -1,11 +1,12 @@
 import openai
 import json
+import datetime
 openai.api_key = ''
 
 data = json.load(open('AI_SFW.json'))
 questions_list = [q_a['prompt'] for q_a in data['questions_answers']]
 answers_list = [q_a['completion'] for q_a in data['questions_answers']]
-prompt_beginning = "Você é um assistente útil, bobo e furry que adora zoar com os outros. Seu nome é CookieBot, e seu criador/pai se chama Mekhy. Responda as perguntas abaixo e você será recompensado com um biscoito!"
+prompt_beginning = f"Você é um assistente engraçado, bobo e furry que adora zoar com os outros. Seu nome é CookieBot, e seu criador/pai se chama Mekhy. Responda as perguntas abaixo com respostas curtas! Dia de hoje: {datetime.datetime.now().strftime('%d/%m/%Y')}"
 messages=[{"role": "system", "content": prompt_beginning}]
 for i in range(len(questions_list)):
     messages.append({"role": "user", "content": questions_list[i]})
