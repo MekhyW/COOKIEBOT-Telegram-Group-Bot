4384504605c11531c29539e67f8ce0d5e0af65fa
Felipe Catapano Emrich Melo <mefkhytheroboticist@gmail.com>
2023-07-05 05:47:42 +0200

    Updated pv message

--- Bot/COOKIEBOT.py
+++ Bot/COOKIEBOT.py
@@ -58,7 +58,7 @@ def thread_function(msg):
             if isBombot:
                 Send(cookiebot, chat_id, "Olá, sou o BomBot!\nSou um clone do @CookieMWbot criado para os chats da Brasil FurFest (BFF)\n\nSe tiver qualquer dúvida ou quiser a lista de comandos completa, mande uma mensagem para o @MekhyW")
             else:
-                Send(cookiebot, chat_id, "Olá, sou o CookieBot!\n\nAtualmente estou presente em *256* chats!\nSinta-se à vontade para me adicionar no seu\n\nSou um bot com IA de conversa, Defesa de grupos, Pesquisa, Conteúdo customizado e Speech-to-text.\nUse /comandos para ver todas as minhas funcionalidades\n\nSe tiver qualquer dúvida ou quiser que algo seja adicionado, mande uma mensagem para o @MekhyW",
+                Send(cookiebot, chat_id, "Olá, sou o CookieBot!\n\nAtualmente estou presente em *256* chats!\nSinta-se à vontade para me adicionar no seu\n\nSou um bot com IA de conversa, Defesa de grupos, Pesquisa, Conteúdo customizado e Publicação automática.\nUse /comandos para ver todas as minhas funcionalidades\n\nSe tiver qualquer dúvida ou quiser que algo seja adicionado, mande uma mensagem para o @MekhyW",
                 reply_markup=InlineKeyboardMarkup(inline_keyboard=[
                     #[InlineKeyboardButton(text="Acesse o Site 🌐", url="https://cookiebot-website.vercel.app/")],
                     [InlineKeyboardButton(text="Adicionar a um Grupo 👋", url="https://t.me/CookieMWbot?startgroup=new")],
