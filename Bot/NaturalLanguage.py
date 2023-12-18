467e1606695a107ceade6a868fc16e323c76b30e
Felipe Catapano Emrich Melo <mefkhytheroboticist@gmail.com>
2023-12-18 02:06:49 -0300

    Update NaturalLanguage.py

--- Bot/NaturalLanguage.py
+++ Bot/NaturalLanguage.py
@@ -60,6 +60,7 @@ def modelNSFW(message, language):
         if any(x > 0.2 for x in [results['hate'], results['hate/threatening'], results['self-harm'], results['self-harm/instructions'], results['self-harm/intent'], results['sexual/minors'], results['violence/graphic']]):
             AnswerFinal = "*" * len(AnswerFinal)
     else:
+        print(r.json())
         AnswerFinal = ""
     return AnswerFinal
 
