b7eb076e942e26724e303b553a8996bc287cfa8b
Felipe Catapano Emrich Melo <mefkhytheroboticist@gmail.com>
2024-01-11 12:22:28 -0300

    Tirlin

--- Bot/Miscellaneous.py
+++ Bot/Miscellaneous.py
@@ -1,7 +1,7 @@
 from universal_funcs import *
 newchat_link = "https://t.me/CookieMWbot?startgroup=new"
 testchat_link = "https://t.me/+mX6W3tGXPew2OTIx"
-num_chats = 429
+num_chats = 434
 
 def decapitalize(s, upper_rest = False):
   return ''.join([s[:1].lower(), (s[1:].upper() if upper_rest else s[1:])])
