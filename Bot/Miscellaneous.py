b387ec902e53144d6a40b0bff2e63046d4a81bd8
Felipe Catapano Emrich Melo <mefkhytheroboticist@gmail.com>
2024-01-23 18:19:39 -0300

    Update Miscellaneous.py

--- Bot/Miscellaneous.py
+++ Bot/Miscellaneous.py
@@ -2,7 +2,7 @@ from universal_funcs import *
 from Publisher import postmail_chat_link
 newchat_link = "https://t.me/CookieMWbot?startgroup=new"
 testchat_link = "https://t.me/+mX6W3tGXPew2OTIx"
-num_chats = 436
+num_chats = 443
 
 def decapitalize(s, upper_rest = False):
   return ''.join([s[:1].lower(), (s[1:].upper() if upper_rest else s[1:])])
@@ -12,7 +12,7 @@ def PvDefaultMessage(cookiebot, msg, chat_id, isBombot):
         if isBombot:
             Send(cookiebot, chat_id, "Olá, eu sou o BomBot!\nSou um clone do @CookieMWbot criado para os grupos do Brasil FurFest (BFF)\n\nSe tiver alguma dúvida ou quiser a lista completa de comandos, mande uma mensagem para @MekhyW")
         else:
-            Send(cookiebot, chat_id, f"Olá, eu sou o CookieBot!\n\nAtualmente estou presente em *{num_chats}* grupos ativos!\nSinta-se livre para me adicionar ao seu :)\n\nSou um bot com IA de Conversação, Defesa de Grupo, Pesquisa, Conteúdo Personalizado e Publicação Automática.\nUse /configurar para alterar minhas configurações (incluindo idioma)\nUse /comandos para ver todas as minhas funcionalidades\n\nSe tiver alguma dúvida ou quiser algo adicionado, mande uma mensagem para @MekhyW",
+            Send(cookiebot, chat_id, f"Olá, eu sou o CookieBot!\n\nAtualmente estou presente em {number_to_emojis(num_chats)} grupos ativos!\nSinta-se livre para me adicionar ao seu :)\n\nSou um bot com IA de Conversação, Defesa de Grupo, Pesquisa, Conteúdo Personalizado e Publicação Automática.\nUse /configurar para alterar minhas configurações (incluindo idioma)\nUse /comandos para ver todas as minhas funcionalidades\n\nSe tiver alguma dúvida ou quiser algo adicionado, mande uma mensagem para @MekhyW",
             reply_markup=InlineKeyboardMarkup(inline_keyboard=[
                 [InlineKeyboardButton(text="Adicionar a um Grupo 👋", url=newchat_link)],
                 [InlineKeyboardButton(text="Mural de Divulgações 📬", url=postmail_chat_link)],
@@ -22,7 +22,7 @@ def PvDefaultMessage(cookiebot, msg, chat_id, isBombot):
         if isBombot:
             Send(cookiebot, chat_id, "Hello, I'm BomBot!\nI'm a clone of @CookieMWbot created for Brasil FurFest (BFF) chats\n\nIf you have any questions or want the complete list of commands, send a message to @MekhyW")
         else:
-            Send(cookiebot, chat_id, f"Hello, I'm CookieBot!\n\nI'm currently present in *{num_chats}* active group chats!\nFeel free to add me to yours :)\n\nI'm a bot with Conversation AI, Group Defense, Search, Custom Content and Auto Publish.\nUse /configure to change my settings (including language)\nUse /commands to see all my features\n\nIf you have any questions or want something added, message @MekhyW",
+            Send(cookiebot, chat_id, f"Hello, I'm CookieBot!\n\nI'm currently present in {number_to_emojis(num_chats)} active group chats!\nFeel free to add me to yours :)\n\nI'm a bot with Conversation AI, Group Defense, Search, Custom Content and Auto Publish.\nUse /configure to change my settings (including language)\nUse /commands to see all my features\n\nIf you have any questions or want something added, message @MekhyW",
             reply_markup=InlineKeyboardMarkup(inline_keyboard=[
                 [InlineKeyboardButton(text="Add me to a Group 👋", url=newchat_link)],
                 [InlineKeyboardButton(text="Shared Posts 📬", url=postmail_chat_link)],
