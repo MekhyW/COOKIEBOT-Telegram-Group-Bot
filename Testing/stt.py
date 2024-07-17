57e1f761b818d1c04a8517e01e04edbe41c4a92d
Felipe Catapano Emrich Melo <mefkhytheroboticist@gmail.com>
2024-07-16 22:03:33 -0300

    Create stt.py

---
+++ Testing/stt.py
@@ -0,0 +1,15 @@
+import openai
+import json
+openai_client = openai.OpenAI(api_key=json.loads(open('Bot/cookiebot_basecredentials.json', 'r').read())['openai_key'])
+
+def Speech_to_text(filename):
+    with open(filename, 'rb') as audio_file:
+        transcript = openai_client.audio.transcriptions.create(
+            model="whisper-1", 
+            file=audio_file, 
+            response_format="text"
+        )
+    transcript = transcript.capitalize()
+    return transcript
+
+print(Speech_to_text('audio_2024-07-16_20-59-44.ogg'))
