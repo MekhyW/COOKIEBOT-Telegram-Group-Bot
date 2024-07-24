a6a6f361c3f79039002891d5532d495723eba1ae
Felipe Catapano Emrich Melo <mefkhytheroboticist@gmail.com>
2024-07-24 18:23:48 -0300

    Update Miscellaneous.py

--- Bot/Miscellaneous.py
+++ Bot/Miscellaneous.py
@@ -262,7 +262,7 @@ def Countdown(cookiebot, msg, chat_id, language, isBombot):
             while daysremaining < -5:
                 daysremaining += 365
             caption = f"<b>Faltam {number_to_emojis(daysremaining)} dias para o FurSMeet {year}!</b>\n\n<i>{calltoaction}</i>\n🦕🦖🦫🦕🦖🦫🦕🦖🦫🦕🦖🦫🦕🦖🦫\n\n📆 {day} a {day+2}/{month}, Santa Maria, Rio Grande do Sul\n🎫Link para comprar ingresso: fursmeet.carrd.co\n💻 Informações no site: fursmeet.wixsite.com/fursmeet\n📲 Grupo do evento: @fursmeet"
-    cookiebot.sendPhoto(chat_id, pic, caption=caption, reply_to_message_id=msg['message_id'], parse_mode='HTML')
+    SendPhoto(cookiebot, chat_id, pic, caption=caption, msg_to_reply=msg, language=language, isBombot=isBombot)
     pic.close()
 
 def Desenterrar(cookiebot, msg, chat_id, thread_id=None):
