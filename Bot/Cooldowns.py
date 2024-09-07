c6e2ace58aec25123a814766102ec3f3ee7a0178
Felipe Catapano Emrich Melo <mefkhytheroboticist@gmail.com>
2024-09-06 22:02:47 -0300

    Update Cooldowns.py

--- Bot/Cooldowns.py
+++ Bot/Cooldowns.py
@@ -2,18 +2,18 @@ from universal_funcs import get_request_backend, post_request_backend, put_reque
 import telepot
 MAX_CONSECUTIVE_RESPONSES_AI = 7
 remaining_responses_ai = {}
+last_used_sticker = {}
 
 def sticker_anti_spam(cookiebot, msg, chat_id, stickerspamlimit, language):
-    sticker_seq = get_request_backend(f"stickers/{chat_id}")
-    if 'error' in sticker_seq and sticker_seq['error'] == "Not Found":
-        post_request_backend(f"stickers/{chat_id}", {"lastUsed": 0})
+    if chat_id not in last_used_sticker:
+        last_used_sticker[chat_id] = 0
     else:
-        last_used = int(sticker_seq['lastUsed']) + 1
+        last_used = int(last_used_sticker[chat_id]) + 1
         if last_used == int(stickerspamlimit):
             send_message(cookiebot, chat_id, "Cuidado com o flood de stickers", msg, language)
         if int(last_used) > int(stickerspamlimit):
             delete_message(cookiebot, telepot.message_identifier(msg))
-        put_request_backend(f"stickers/{chat_id}", {"lastUsed": last_used})
+        last_used_sticker[chat_id] = last_used
 
 def increase_remaining_responses_ai(user_id):
     if user_id in remaining_responses_ai:
@@ -30,4 +30,4 @@ def decrease_remaining_responses_ai(user_id):
         remaining_responses_ai[user_id] = MAX_CONSECUTIVE_RESPONSES_AI - 1
 
 def sticker_cooldown_updates(chat_id):
-    put_request_backend(f"stickers/{chat_id}", {"lastUsed": 0})
+    last_used_sticker[chat_id] = 0
