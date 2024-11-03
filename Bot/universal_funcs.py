3689eb92cdc6cd74c25e165150833c97030f2845
Felipe Catapano Emrich Melo <mefkhytheroboticist@gmail.com>
2024-11-02 22:39:21 -0300

    Update universal_funcs.py

--- Bot/universal_funcs.py
+++ Bot/universal_funcs.py
@@ -144,9 +144,11 @@ def send_message(cookiebot, chat_id, text, msg_to_reply=None, language="pt", thr
                 text = text.replace('\\', '').replace('>', '')
                 cookiebot.sendMessage(chat_id, text, reply_markup=reply_markup)
     except urllib3.exceptions.ProtocolError:
+        print(traceback.format_exc())
         send_message(cookiebot, chat_id, text, msg_to_reply, language, 
                      thread_id, is_alternate_bot, reply_markup, parse_mode)
     except TelegramError:
+        print(traceback.format_exc())
         try:
             cookiebot.sendMessage(ownerID, traceback.format_exc())
         except Exception as e:
