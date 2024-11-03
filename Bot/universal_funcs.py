65404cffe6d169d500203b123328344bd5a2159b
Felipe Catapano Emrich Melo <mefkhytheroboticist@gmail.com>
2024-11-02 22:56:35 -0300

    Update universal_funcs.py

--- Bot/universal_funcs.py
+++ Bot/universal_funcs.py
@@ -147,14 +147,10 @@ def send_message(cookiebot, chat_id, text, msg_to_reply=None, language="pt", thr
         print(traceback.format_exc())
         send_message(cookiebot, chat_id, text, msg_to_reply, language, 
                      thread_id, is_alternate_bot, reply_markup, parse_mode)
-    except TelegramError:
-        print(traceback.format_exc())
-        try:
-            cookiebot.sendMessage(ownerID, traceback.format_exc())
-        except Exception as e:
-            print(e)
     except Exception as e:
-        print(e)
+        print(traceback.format_exc())
+        print(text)
+        cookiebot.sendMessage(ownerID, traceback.format_exc())
 
 def send_photo(cookiebot, chat_id, photo, caption=None, msg_to_reply=None, language="pt", thread_id=None, is_alternate_bot=0, reply_markup=None):
     try:
