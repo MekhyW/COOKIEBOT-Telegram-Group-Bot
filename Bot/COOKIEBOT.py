c6d13b780404a763fa89a352fa85961acae497be
Felipe Catapano Emrich Melo <mefkhytheroboticist@gmail.com>
2025-03-17 13:38:15 -0300

    Update COOKIEBOT.py

--- Bot/COOKIEBOT.py
+++ Bot/COOKIEBOT.py
@@ -384,11 +384,11 @@ def thread_function_query(msg):
                 return
             if query_data.split()[1].isnumeric():
                 delete_message(cookiebot, telepot.message_identifier(msg['message']))
-                giveaways_create(cookiebot, msg, int(query_data.split()[1]), chat_id, " ".join(query_data.replace('"', '').split()[2:]))
+                giveaways_create(cookiebot, msg, int(query_data.split()[1]), msg['message']['chat']['id'], " ".join(query_data.replace('"', '').split()[2:]))
             elif query_data.split()[1] == 'enter':
-                giveaways_enter(cookiebot, msg, chat_id)
+                giveaways_enter(cookiebot, msg, msg['message']['chat']['id'])
             elif query_data.split()[1] == 'end':
-                giveaways_end(cookiebot, msg, chat_id, listaadmins_id)
+                giveaways_end(cookiebot, msg, msg['message']['chat']['id'], listaadmins_id)
             else:
                 cookiebot.answerCallbackQuery(query_id, text="ERROR! please contact @MekhyW")
         else:
