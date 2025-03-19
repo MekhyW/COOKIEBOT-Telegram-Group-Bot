d5ce9d9994d9daf84f69946370c5d33c1bfab035
Felipe Catapano Emrich Melo <mefkhytheroboticist@gmail.com>
2025-03-19 12:31:37 -0300

    Update universal_funcs.py

--- Bot/universal_funcs.py
+++ Bot/universal_funcs.py
@@ -116,12 +116,16 @@ def translate(text, dest='en'):
         count += 1
         return placeholder
     newline_token = " NEWLINE_TOKEN_XYZ "
+    newline_token_alts = ["NEWLINE_TOKEN_XYZ", "NEWLINE_XYZ_XYZ", "newline_xyz", "token_xyz", "_TOKEN_TOKKN_XYZ", "NEWLINE_TOKN_XYZ", "newline_xyz_token_xyz_xyz", "newloken_token_xyz", "_xyz", "newlina_TOKEN_XYZ"]
     text_with_placeholders = re.sub(r'<[^>]*>', replace_tag, text)
     text_with_tokens = text_with_placeholders.replace('\n', newline_token)
     translated = GoogleTranslator(source='auto', target=dest[:2]).translate(text_with_tokens)
     for placeholder, tag in tags.items():
         translated = re.sub(re.escape(placeholder.strip()), tag, translated, flags=re.IGNORECASE)
-    translated = re.sub(re.escape(newline_token.strip()), '\n', translated, flags=re.IGNORECASE)
+    for token in newline_token_alts:
+        padded_token = f" {token} "
+        translated = re.sub(re.escape(padded_token.strip()), '\n', translated, flags=re.IGNORECASE)
+        translated = re.sub(re.escape(token), '\n', translated, flags=re.IGNORECASE)
     translated = translated.replace('R $', 'R$')
     return translated
 
